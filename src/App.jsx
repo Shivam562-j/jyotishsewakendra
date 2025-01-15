9baef4ef6199cb96edb23747a08e18ac01cce276
Shivam Kumar <76731566+Shivam562-j@users.noreply.github.com>
2025-01-15 22:27:36 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <h1 className='bg-orange-600 h-screen font-semibold flex items-center justify-center text-[55px] text-white'>Comming Soon</h1>
+  )
+}
+
+export default App
