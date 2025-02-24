407c0fba5c46dd12e51e89058cd6fd029a6e75bc
shivam562-j <krshivam998@gmail.com>
2025-02-24 23:07:52 +0530

    loader add

--- src/components/ComingSoon.jsx
+++ src/components/ComingSoon.jsx
@@ -4,20 +4,29 @@ import bgLoader from '../assets/images/loader.png'
 
 const ComingSoon = () => {
   return (
-    <div className="relative bg-[#0f0221] h-screen font-semibold flex items-center justify-center text-[55px] text-white text-center">
 
-      <img src={bgImg} alt="bg" className="absolute w-auto h-[550px] object-cover text-center" />
+    <div className="bg-[#0f0221] h-screen flex flex-col items-center justify-center px-4 w-full overflow-hidden">
+      <div className="relative  flex flex-col items-center justify-center px-4 w-full">
 
-      <img
-        src={bgLoader}
-        alt="loader-bg"
-        className="absolute animate-spin-slow w-[500px] h-[500px]"
-      />
+        {/* Background Image */}
+        <img
+          src={bgImg}
+          alt="bg"
+          className="absolute w-auto h-[200px] sm:h-[350px] md:h-[450px] lg:h-[550px] object-cover"
+        />
 
-      <div className="relative animate-typing overflow-hidden whitespace-nowrap border-r-4 border-white">
-        Coming Soon...
+        {/* Spinning Loader */}
+        <img
+          src={bgLoader}
+          alt="loader-bg"
+          className="absolute animate-spin-slow w-[200px] h-[200px] sm:w-[300px] sm:h-[300px] md:w-[400px] md:h-[400px] object-contain"
+        />
+
       </div>
 
+      <div className="relative w-fit font-semibold text-[30px] sm:text-[45px] md:text-[55px] lg:text-[65px] animate-typing overflow-hidden whitespace-nowrap border-r-4 border-white text-white text-center ">
+        Coming Soon...
+      </div>
     </div>
   )
 }
