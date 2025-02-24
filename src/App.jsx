2f559383d85bc8291e16fa157fc9eacb77391827
Shivam Kumar <76731566+Shivam562-j@users.noreply.github.com>
2025-02-24 20:17:13 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 function App() {
 
   return (
-    <h1 className='bg-orange-600 h-screen font-semibold flex items-center justify-center text-[55px] text-white text-center'>Comming Soon</h1>
+    <h1 className='bg-orange-600 h-screen font-semibold flex items-center justify-center text-[55px] text-white text-center'>Coming Soon</h1>
   )
 }
 
