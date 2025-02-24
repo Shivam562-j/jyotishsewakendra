795b041338a90812a7d9172873fa63475565ebec
shivam562-j <krshivam998@gmail.com>
2025-02-24 21:54:32 +0530

    app router done

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import ComingSoon from '../components/ComingSoon';
-import Home from '../pages/Home/Home';
+import { AboutUs, Appointment, ContactUs, Gallery, Home, Services, ThankYou } from '../pages';
 
 const AppRouter = () => {
   return (
@@ -9,6 +9,13 @@ const AppRouter = () => {
         <Routes>
             <Route path='/' element={<ComingSoon />} />
             <Route path='/home' element={<Home />} />
+            <Route path='/about-us' element={<AboutUs />} />
+            <Route path='/services' element={<Services />} />
+            <Route path='/appointment' element={<Appointment />} />
+            <Route path='/gallery' element={<Gallery />} />
+            <Route path='/contact-us' element={<ContactUs />} />
+            <Route path='/thank-you' element={<ThankYou />} />
+
         </Routes>
     </Router>
   )
