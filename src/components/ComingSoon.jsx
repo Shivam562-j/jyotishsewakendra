16a975cfa043595032d9a9ae25cbfd4897f2d537
shivam562-j <krshivam998@gmail.com>
2025-02-24 22:34:00 +0530

    coming soon loder page and app router done

--- src/components/ComingSoon.jsx
+++ src/components/ComingSoon.jsx
@@ -1,8 +1,24 @@
 import React from 'react';
+import bgImg from '../assets/images/hand-marks-service_img.png'
+import bgLoader from '../assets/images/loader.png'
 
 const ComingSoon = () => {
   return (
-    <h1 className='bg-orange-600 h-screen font-semibold flex items-center justify-center text-[55px] text-white text-center'>Coming Soon</h1>
+    <div className="relative bg-[#0f0221] h-screen font-semibold flex items-center justify-center text-[55px] text-white text-center">
+
+      <img src={bgImg} alt="bg" className="absolute w-auto h-[550px] object-cover text-center" />
+
+      <img
+        src={bgLoader}
+        alt="loader-bg"
+        className="absolute animate-spin-slow w-[500px] h-[500px]"
+      />
+
+      <div className="relative animate-typing overflow-hidden whitespace-nowrap border-r-4 border-white">
+        Coming Soon...
+      </div>
+
+    </div>
   )
 }
 
