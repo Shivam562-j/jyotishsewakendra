86cff2b6e9e2ad4911824cef9cb3a1832b4a413e
shivam562-j <krshivam998@gmail.com>
2025-03-20 23:09:12 +0530

    changes

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -8,9 +8,9 @@ import Cta from '../pages/Components/Cta';
 
 const Layout = () => {
   const location = useLocation();
-  const isShowReview = ['/home', '/about-us', '/services', '/gallery', '/gallery/our-video', '/gallery/our-products'];
+  const isShowReview = ['/', '/home', '/about-us', '/services', '/gallery', '/gallery/our-video', '/gallery/our-products'];
 
-  const isShowCta = ['/home', '/about-us', '/services', "/services/janam-kundali", "/services/kundali-vishleshan", "/services/child-astrology", "/services/future-astrology", "/services/marriage-astrology", "/services/vastu-shastra", '/gallery', '/gallery/our-video', '/gallery/our-products'];
+  const isShowCta = ['/', '/home', '/about-us', '/services', "/services/janam-kundali", "/services/kundali-vishleshan", "/services/child-astrology", "/services/future-astrology", "/services/marriage-astrology", "/services/vastu-shastra", '/gallery', '/gallery/our-video', '/gallery/our-products'];
 
   const isShowBanner = ['/about-us', '/services', "/services/janam-kundali", "/services/kundali-vishleshan", "/services/child-astrology", "/services/future-astrology", "/services/marriage-astrology", "/services/vastu-shastra", '/gallery', '/gallery/our-video', '/gallery/our-products', '/appointment', '/contact-us', '/thank-you'];
 
