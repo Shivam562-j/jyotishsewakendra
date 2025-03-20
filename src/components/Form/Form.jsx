625e7828a7f1d0505ef52ca51c68eb076f211914
shivam562-j <krshivam998@gmail.com>
2025-03-20 23:38:08 +0530

    changes

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import Button from '../Button/Button';
 
-const Form = ({ isContactForm = false }) => {
+const Form = ({ isContactForm = false, isServicePage  }) => {
     const [formData, setFormData] = useState({
         firstName: "",
         lastName: "",
